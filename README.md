bd641cf0ae7bb378bd63c8b9d44642a4c233570b
Andrew <63522731+aloutfi@users.noreply.github.com>
2022-04-08 21:22:07 -0500

    feat: initial working version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # allergyAlly-geo
+
+```mermaid
+sequenceDiagram
+    participant AA_Client
+    participant AA_Geo
+    participant Google_Maps
+    AA_Client->>AA_Geo: Get me coordinates <br> for the given this address
+    AA_Geo->>Google_Maps: How about you?
+```
---
+++ aa-geo.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+import dotenv  from "dotenv";
+
+dotenv.config({path:'app.env'})
+
+function geocodeAddress(address) {
+    /*
+    Given an address, return the coordinates for that address
+    */
+    const geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    const url = `${geocode_url}address=${address}&key=${process.env.GCP_API_KEY}`
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data.results[0].geometry.location)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
