a8437c332c27eb48cf54cecbb489269e6a8b94a9
Andrew <63522731+aloutfi@users.noreply.github.com>
2022-04-08 22:23:06 -0500

    feat: standalone method. stable

--- aa-geo.js
+++ aa-geo.js
@@ -3,18 +3,21 @@ import dotenv  from "dotenv";
 
 dotenv.config({path:'app.env'})
 
-export function geocodeAddress(address) {
+async function geocodeAddress(address) {
     /*
     Given an address, return the coordinates for that address
     */
     const geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
     const url = `${geocode_url}address=${address}&key=${process.env.GCP_API_KEY}`
-    fetch(url)
+    const response = fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data.results[0].geometry.location);
+            return data.results[0].geometry.location;
         })
         .catch(err => {
             console.log(`error ${err}`);
-        });
+        })
+        return response
 }
+
+console.log(await geocodeAddress('915 20th ave NE Minneapolis, MN 55418'));
